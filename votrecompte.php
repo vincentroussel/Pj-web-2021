9c22d12bdbdfad86e991b092bfe1ecc477927909
LyesSadali <78852584+LyesSadali@users.noreply.github.com>
2021-05-25 16:01:39 +0200

    Add files via upload

---
+++ votrecompte.php
@@ -0,0 +1,14 @@
+<?php
+	$mail = isset($_POST["mail"])? $_POST["mail"] : "";
+	$motdepasse = isset($_POST["password"])? $_POST["password"] : "";
+
+	$erreur = "";
+	if ($mail == "") 
+		{ $erreur .= "Le champ Mail est vide. <br>"; }
+	if ($motdepasse == "") 
+		{$erreur .= "Le champ Mot de Passe est vide. <br>";}
+	if ($erreur == "") 
+		{ echo "Formulaire valide.";}
+
+	else { echo "Erreur: <br>" . $erreur; }
+	?>
