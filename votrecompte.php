8a1bcb9cc17dadeebe337e57d0dfe87ce40670b3
Arlouys <78851920+Arlouys@users.noreply.github.com>
2021-05-27 15:53:14 +0200

    Modification de votre compte.php avec SESSION

--- votrecompte.php
+++ votrecompte.php
@@ -16,31 +16,41 @@
 		if ($typecompte == 'admin') {
 			$sql = 'SELECT * FROM vendeurs WHERE admin = 1'; 
 		}
+		if ($typecompte == '') {
+			$URL = 'votrecompte.';
+		}else{
 		$verif = 0;
 		$result = mysqli_query($db_handle,$sql);
 		while ($data = mysqli_fetch_assoc($result)){
 			if (($data['Mail'] == $mail) && ($data['Motdepasse'] == $motdepasse)) {
+				$_SESSION['sessionID'] = $data['ID'];
 				$verif = 1;
 				$mem = $data['ID'];
 			}
 		}
 		if ($verif == 0) {
-			header('Location : votrecompte.html');
-			exit();
+			$URL = 'votrecompte.';
+			//header('Location : votrecompte.html');
 		}else{
-			$_SESSION['IDsession'] = $mem;
-			if ($typecompte == 'acheteur') {
-				header('Location : accueilAcheteur.html');
-				exit();
-			}
-			if ($typecompte == 'vendeur') {
-				header('Location : pagevendeur.html');
-				exit();
-			}
-			if ($typecompte == 'admin') {
-				header('Location : pageadmin.html');
-				exit();
-			}
+				$_SESSION['IDsession'] = $mem;
+				if ($typecompte == 'acheteur') {
+					//echo "acheteur";
+					$URL = 'accueilAcheteur.';
+					//header('Location : accueilAcheteur.html');
+				}
+				if ($typecompte == 'vendeur') {
+					//echo "vendeur";
+					$URL = 'pagevendeur.';
+					//header('Location : pagevendeur.html');
+				}
+				if ($typecompte == 'admin') {
+					//echo "admin";
+					$URL = 'pageadmin.';
+					//header('Location : pageadmin.html');
+				}
+			}	
 		}
-	}
+		
+	}header('Location: '.$URL.html);
+	exit();
 	?>
