0f82204bed49aee4c6f39eac2d86e27b02a7a05f
Arlouys <78851920+Arlouys@users.noreply.github.com>
2021-05-28 23:43:48 +0200

    Ajout du nom et prenom dans le form
    et correction des bugs que vincent n'a pas vérifié

--- supprimervendeur.php
+++ supprimervendeur.php
@@ -1,13 +1,14 @@
 <?php
 	$mail = isset($_POST["mail"])? $_POST["mail"] : "";
+	$nom = isset($_POST["nom"])? $_POST["nom"] : "";
+	$prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 	$database = "ecemarketplace";
 	$db_handle = mysqli_connect('localhost', 'root', '' );
 	$db_found = mysqli_select_db($db_handle, $database);
 	if ($db_found) {
-		$sql = "DELETE * FROM vendeurs WHERE Mail=$mail";
+		$sql = "DELETE FROM vendeurs WHERE Mail='$mail' AND Nom='$nom' AND Prenom='$prenom'";
+		var_dump($sql);
 		$result = mysqli_query($db_handle,$sql);
-
-	}
 	}else {
  			echo "Database not found";
 		}
