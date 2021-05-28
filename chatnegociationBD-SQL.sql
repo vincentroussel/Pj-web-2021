317cc415e19ea30be470b84ef3685e0e8dd53440
Arlouys <78851920+Arlouys@users.noreply.github.com>
2021-05-28 11:22:00 +0200

    ajout de la BDD Chat negociation

---
+++ chatnegociationBD-SQL.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 09, 2020 at 10:47 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecemarketplace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatnegociation`
+--
+
+DROP TABLE IF EXISTS `chatnegociation`;
+CREATE TABLE IF NOT EXISTS `chatnegociation` (
+  `ID` int(11) NOT NULL,
+  `IDnegociation` int(11),
+  `message` varchar(255),
+  PRIMARY KEY (`ID`)
+) /*ENGINE=MyISAM AUTO_INCREMENT=1*/ DEFAULT CHARSET=latin1;
