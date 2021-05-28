c943f0e56d30f1fa5a68328242c3bd727dbce337
Arlouys <78851920+Arlouys@users.noreply.github.com>
2021-05-28 13:43:51 +0200

    Ajout de l'HTML (à corriger)

--- misevente.php
+++ misevente.php
@@ -1,73 +1,153 @@
-<?php
-//identifier le nom de base de données
-echo "<meta charset=\"utf-8\">";
-$database = "ecemarketplace";
-//connectez-vous dans votre BDD
-//Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-$db_handle = mysqli_connect('localhost', 'root', '' );
-$db_found = mysqli_select_db($db_handle, $database);
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mise en vente</title>
+  <meta charset="utf-8">
+  <meta name= "viewport" content= "width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel= "stylesheet"href= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+  <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.j s"> </script>
+  <script src= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/j s/bootstrap.min.j s"> </script>
+  <style type="text/css">
+    .navbar {
+         margin-bottom: 0;
+         border-radius: 0;
+      }
+    #footer{
+      background-color: #17A2B8;
+      color: white;
+    }
+  </style>
+</head>
+<body>
+	<div class="container-fluid">
+    <div class="row; text-white bg-info">
+      <h1 class="text-center">Bienvenue sur votre espace de mise en vente de l'ECE MarketPlace <img src="ecemarketplace.jpg"></h1>
+    </div>
+    <div class="row; text-white bg-info">
+      <p>L'ECE MarketPlace est un site de vente en ligne pour la communauté ECE.<br> Vendez ou bien achetez, des produits de bonne qualité en utilisant nos diverses méthodes : négociation, vente aux enchères ou tout simplement en achat/vente instantané(e).<br> ECE MarketPlace, la plateforme de vente où tout devient possible.</p>
+    </div>
+  <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>                        
+          </button>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-expand">
+                <li><a href="pagevendeur.html">Accueil</a></li>
+                <li><a href="#">Evaluer mon bien</a></li>
+                <li  class="active"><a href="misevente.html">Vendre un bien</a></li>
+                <li><a href="espacevente.html">Mon espace de vente</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right navbar-expand">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="votrecompte.html" id="navbarDropdownMenuLink" data-toggle="dropdown"aria-haspopup="true"aria-expanded="false">Votre Compte</a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="profilvendeur.html">Mon Profil</a>
+                    <a class="dropdown-item" href="votrecompte.html">Déconnexion</a>
+                  </div>
+                </li>
+              </ul>
+          </div>
+        </div>
+    </nav>
+    <br>
+	<br>
+	<?php
+	//identifier le nom de base de données
+	echo "<meta charset=\"utf-8\">";
+	$database = "ecemarketplace";
+	//connectez-vous dans votre BDD
+	//Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
+	$db_handle = mysqli_connect('localhost', 'root', '' );
+	$db_found = mysqli_select_db($db_handle, $database);
 
-//saisir les données de notre formulaire
-$nom = isset($_POST["nom"])? $_POST["nom"] : "";
-$prix = isset($_POST["prix"])? $_POST["prix"] : "";
-$qualites = isset($_POST["qualites"])? $_POST["qualites"] : "";
-$defauts = isset($_POST["defauts"])? $_POST["defauts"] : "";
-$ville= isset($_POST["ville"])? $_POST["ville"] : "";
-$categorievente= isset($_POST["categorievente"])? $_POST["categorievente"] : "";
-$categorieobjet= isset($_POST["categorieobjet"])? $_POST["categorieobjet"] : "";
+	//saisir les données de notre formulaire
+	$nom = isset($_POST["nom"])? $_POST["nom"] : "";
+	$prix = isset($_POST["prix"])? $_POST["prix"] : "";
+	$qualites = isset($_POST["qualites"])? $_POST["qualites"] : "";
+	$defauts = isset($_POST["defauts"])? $_POST["defauts"] : "";
+	$ville= isset($_POST["ville"])? $_POST["ville"] : "";
+	$categorievente= isset($_POST["categorievente"])? $_POST["categorievente"] : "";
+	$categorieobjet= isset($_POST["categorieobjet"])? $_POST["categorieobjet"] : "";
 
- //si le BDD existe, faire le traitement
-if ($db_found) {
-	//on va chercher le produit dans la BDD
-	$sql = "SELECT * FROM objets WHERE Nom LIKE '%$nom'";
-	$result = mysqli_query($db_handle, $sql);
-	//on regarde si il y'a des résultats
-	if (mysqli_num_rows($result) != 0) {
-		//Un article du même nom existe déjà
-		echo "Un article du même nom existe déjà. <br>";
-	} else{
-		//on ajoute l'article dans la BDD
-		$sql = "INSERT INTO objets(Nom, Prix, Defauts, Qualites, Typevente, Categorie) VALUES('$nom', $prix ,'$defauts','$qualites','$categorievente','$categorieobjet')";
-		$result = mysqli_query($db_handle,$sql);
-		echo "Ajout confirmé <br>";
-		//on affiche l'objet ajouté
+	 //si le BDD existe, faire le traitement
+	if ($db_found) {
+		//on va chercher le produit dans la BDD
 		$sql = "SELECT * FROM objets WHERE Nom LIKE '%$nom'";
 		$result = mysqli_query($db_handle, $sql);
-		echo "<h4>Informations sur le nouveau objet ajouté:</h4>";
-		echo "<table border='1'>";
-		echo "<tr>";
-		echo "<th>" . "ID" . "</th>";
-		echo "<th>" . "Nom" . "</th>";
-		echo "<th>" . "Prix" . "</th>";
-		echo "<th>" . "Defauts" . "</th>";
-		echo "<th>" . "Qualites" . "</th>";
-		echo "<th>" . "Type de vente" . "</th>";
-		echo "<th>" . "Categorie" . "</th>";
-		echo "</tr>";
-		while ($data = mysqli_fetch_assoc($result)) {
+		//on regarde si il y'a des résultats
+		if (mysqli_num_rows($result) != 0) {
+			//Un article du même nom existe déjà
+			echo "Un article du même nom existe déjà. <br>";
+		} else{
+			//on ajoute l'article dans la BDD
+			$sql = "INSERT INTO objets(Nom, Prix, Defauts, Qualites, Typevente, Categorie) VALUES('$nom', $prix ,'$defauts','$qualites','$categorievente','$categorieobjet')";
+			$result = mysqli_query($db_handle,$sql);
+			echo "Ajout confirmé <br>";
+			//on affiche l'objet ajouté
+			$sql = "SELECT * FROM objets WHERE Nom LIKE '%$nom'";
+			$result = mysqli_query($db_handle, $sql);
+			echo "<h4>Informations sur le nouveau objet ajouté:</h4>";
+			echo "<table border='1'>";
 			echo "<tr>";
-			echo "<th>" . $data['ID'] . "</th>";
-			echo "<th>" . $data['Nom'] . "</th>";
-			echo "<th>" . $data['Prix'] . "</th>";
-			echo "<th>" . $data['Defauts'] . "</th>";
-			echo "<th>" . $data['Qualites'] . "</th>";
-			echo "<th>" . $data['Typevente'] . "</th>";
-			echo "<th>" . $data['Categorie'] . "</th>";
+			echo "<th>" . "ID" . "</th>";
+			echo "<th>" . "Nom" . "</th>";
+			echo "<th>" . "Prix" . "</th>";
+			echo "<th>" . "Defauts" . "</th>";
+			echo "<th>" . "Qualites" . "</th>";
+			echo "<th>" . "Type de vente" . "</th>";
+			echo "<th>" . "Categorie" . "</th>";
 			echo "</tr>";
+			while ($data = mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+				echo "<th>" . $data['ID'] . "</th>";
+				echo "<th>" . $data['Nom'] . "</th>";
+				echo "<th>" . $data['Prix'] . "</th>";
+				echo "<th>" . $data['Defauts'] . "</th>";
+				echo "<th>" . $data['Qualites'] . "</th>";
+				echo "<th>" . $data['Typevente'] . "</th>";
+				echo "<th>" . $data['Categorie'] . "</th>";
+				echo "</tr>";
+			}
+			$sql = "SELECT ID FROM objets WHERE Nom LIKE '%$nom'";
+			$result = mysqli_query($db_handle, $sql);
+			while ($data = mysqli_fetch_assoc($result)){
+				$ID = $data['ID'];
+			}
+			$sql = "UPDATE objets SET IDimages = $ID WHERE ID = $ID;";
+			$result = mysqli_query($db_handle,$sql);
 		}
-		$sql = "SELECT ID FROM objets WHERE Nom LIKE '%$nom'";
-		$result = mysqli_query($db_handle, $sql);
-		while ($data = mysqli_fetch_assoc($result)){
-			$ID = $data['ID'];
-		}
-		$sql = "UPDATE objets SET IDimages = $ID WHERE ID = $ID;";
-		$result = mysqli_query($db_handle,$sql);
-	}
-}//end if
-//si le BDD n'existe pas
-else {
- echo "Database not found";
-}//end else
-//fermer la connection
-mysqli_close($db_handle);
-?>
+	}//end if
+	//si le BDD n'existe pas
+	else {
+	 echo "Database not found";
+	}//end else
+	//fermer la connection
+	mysqli_close($db_handle);
+	?>
+    <br>
+	<br>
+	<div id="footer">Copyright &copy; ECE MarketPlace 2021<br>
+      <p>Vous pouvez-nous contacter :
+      <ul>
+        <li>Par téléphone au <em>(+33) 07 60 52 04 07</em></li>
+        <li>Par email à <a href="mailto:ece.marketplace@gmail.com">ece.marketplace@gmail.com </a></li>
+        <li>ou rendez-vous chez nous au <address>37 Quai de Grenelle, 75015 Paris</address></li>
+      </ul>
+      </p>
+    </div>
+</body>
+</html>
