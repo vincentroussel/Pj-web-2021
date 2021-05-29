974254c6b84bd33a0b6a9f6be305c3318c71a777
Arlouys <78851920+Arlouys@users.noreply.github.com>
2021-05-30 00:32:01 +0200

    etoile supprimée dans le DELETE

--- supprimerobjet.php
+++ supprimerobjet.php
@@ -41,7 +41,7 @@
 	$db_handle = mysqli_connect('localhost', 'root', '' );
 	$db_found = mysqli_select_db($db_handle, $database);
 	if ($db_found) {
-		$sql = "DELETE * FROM objets WHERE ID=$ID";
+		$sql = "DELETE FROM objets WHERE ID=$ID";
 		$result = mysqli_query($db_handle,$sql);
 		echo "suppression confirmée <br>";
 
