d6c01db451a2edb842f0a1bd7bbe08f8cb89dad2
Arlouys <78851920+Arlouys@users.noreply.github.com>
2021-05-29 23:43:53 +0200

    ajout nbetape

--- negociationBD-SQL.sql
+++ negociationBD-SQL.sql
@@ -36,5 +36,6 @@ CREATE TABLE IF NOT EXISTS `negociation` (
   `prixacheteur` int(255),
   `IDvendeurs` int(11),
   `IDacheteurs` int(11),
+  `nbetape` int(11),
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
